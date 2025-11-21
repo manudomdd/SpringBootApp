1afb11b04b6a225fd181aae0b98b9bf9e52a4fe9
Manu <dominguez.dieguez.manuel.jesus@iescamas.es>
2025-11-21 20:21:31 +0100

    Ejercicio

---
+++ src/test/java/com/example/MonitorFilosofos/CenaFilosofosAppTests.java
@@ -0,0 +1,13 @@
+package com.example.MonitorFilosofos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenaFilosofosAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
